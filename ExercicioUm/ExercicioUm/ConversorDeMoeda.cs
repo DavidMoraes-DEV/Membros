0621ea3c1b6de57b073f21ae0666a2348cd22c9c
David Moraes <investtraderrs@gmail.com>
2020-09-16 18:06:38 -0300

    Membros de instância e estáticos

---
+++ ExercicioUm/ExercicioUm/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioUm
+{
+    class ConversorDeMoeda
+    {
+        CultureInfo CI = CultureInfo.InvariantCulture;
+
+        public static double Cotacao;
+        public static double TotalEmDolar;
+
+        public static double Conversor()
+        {
+            return ((TotalEmDolar*6.0)/100+TotalEmDolar)*Cotacao;
+        }
+    }
+}
---
+++ ExercicioUm/ExercicioUm/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioUm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            Console.Write("Qual é a cotação do dólar? ");
+            ConversorDeMoeda.Cotacao = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            ConversorDeMoeda.TotalEmDolar = double.Parse(Console.ReadLine(), CI);
+
+            Console.Write("Valor a ser pago em reais = " + ConversorDeMoeda.Conversor().ToString("F2", CI));
+        }
+    }
+}
